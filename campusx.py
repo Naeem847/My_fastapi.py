b0487fa9bff9c8f1b594b35dbc0db0f179083c29
Naeem847 <ashoornaeem@gmail.com>
2026-07-03 13:10:01 +0500

    A simple Patient Management System API built with FastAPI for managing and viewing patient records stored in a JSON file.

--- campusx.py
+++ campusx.py
@@ -1,15 +1,26 @@
+import json
+
 from fastapi import FastAPI
 
 # create a bject of fastapi
 app=FastAPI()
+def load_data():
+    with open("patients.json") as f:
+      data = json.load(f)
+
+    return data
 
 @app.get("/")
 
 def hello():
-    return {"message":"Hello, World!"}
+    return {"message":"patient management system API"}
 
 @app.get("/about")
 
 def about():
-    return {"message":"campusx is an education plateform where you can learn Ai"}
+    return {"message":"A fully functional  API to manage your patient records built with FastAPI."}
 
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
