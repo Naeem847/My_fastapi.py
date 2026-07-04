e1c21723aaf394215e28a9f7da3ce9213e89004c
Naeem847 <ashoornaeem@gmail.com>
2026-07-04 19:00:16 +0500

    A simple Patient Management REST API built with FastAPI and Python. The project demonstrates RESTful API development by loading patient records from a JSON file and providing endpoints to view all patients, retrieve patient details by ID, and serve API information.

--- campusx.py
+++ campusx.py
@@ -1,11 +1,16 @@
 import json
 
+# from fastapi import FastAPI,Path
 from fastapi import FastAPI
 
 # create a bject of fastapi
+
 app=FastAPI()
+
 def load_data():
+
     with open("patients.json") as f:
+      
       data = json.load(f)
 
     return data
@@ -24,3 +29,18 @@ def about():
 def view():
     data = load_data()
     return data
+
+# make the end point to get patient by id
+@app.get('/patient/{patient_id}')
+
+def view_patient(patient_id: str):
+
+    data = load_data()
+
+    # if patient_id in data:  
+    if patient_id['patient_id'] == patient_id:
+
+        return data[patient_id]
+    
+    return {"error":"patient not found"}
+
