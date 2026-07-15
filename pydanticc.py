a8578ae3842b2f2159e013d1c8abce85fba16d95
Naeem847 <ashoornaeem@gmail.com>
2026-07-15 17:07:06 +0500

    In this project, I practiced the core concepts of Pydantic by building a patient data model. I learned how to use Optional for optional fields, Annotated with Field() to add validation and metadata, AnyUrl to validate URLs, and List and Dict to handle complex data types. This project helped me understand how Pydantic validates and manages structured data in Python applications.

--- pydanticc.py
+++ pydanticc.py
@@ -1,36 +1,36 @@
-# # def insert_patient_data(name:str, age:int):
+def insert_patient_data(name:str, age:int):
 
-# #     if type(name) == str and type(age) == int:
-# #        if age<0:
-# #             raise ValueError("Age cannot be negative")
-# #        else:
+     if type(name) == str and type(age) == int:
+       if age<0:
+            raise ValueError("Age cannot be negative")
+       else:
 
-# #         print(name)
-# #         print(age)
+        print(name)
+        print(age)
 
-# #         print("insert into database")
-# #     else:
-#         raise TypeError("Incorrect data type")  
+        print("insert into database")
+     else:
+        raise TypeError("Incorrect data type")  
       
-# # insert_patient_data('ali', 25)
+insert_patient_data('ali', 25)
 
 
-# # # update patient data
+# # update patient data
 
-# # def update_patient_data(name:str, age:int):
+def update_patient_data(name:str, age:int):
 
-# #     if type(name) == str and type(age) == int:
-# #          if age<0:
-# #                 raise ValueError("Age cannot be negative")
-# #          else:
-# #           print(name)
-# #           print(age)
+    if type(name) == str and type(age) == int:
+         if age<0:
+          raise ValueError("Age cannot be negative")
+         else:
+          print(name)
+          print(age)
 
-# #           print("update in database")
-# #     else:
-# #         raise TypeError("Incorrect data type")  
+          print("update in database")
+    else:
+            raise TypeError("Incorrect data type")  
       
-# # update_patient_data('ali', 25)
+update_patient_data('ali', 25)
    
 from pydantic import BaseModel,AnyUrl,Field
 
